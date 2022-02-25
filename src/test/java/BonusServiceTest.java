b7bdc3f3f54a2cb5af86664fd4f469e502043584
kazlipop <88086013+kazlipop@users.noreply.github.com>
2022-02-25 11:59:10 +0300

    Update BonusServiceTest.java

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -34,12 +34,13 @@ public class BonusServiceTest {
         assertEquals(expected, actual);
     }
 
-    {
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForRegisteredAndOverLimit() {
         BonusService service = new BonusService();
 
         // подготавливаем данные:
         long amount = 1000_60;
-        boolean registered = false;
+        boolean registered = true;
         long expected = 10;
 
         // вызываем целевой метод:
@@ -48,12 +49,14 @@ public class BonusServiceTest {
         // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
     }
-    {
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForRegisteredAndOverLimit() {
         BonusService service = new BonusService();
 
         // подготавливаем данные:
         long amount = 1_000_000_60;
-        boolean registered = false;
+        boolean registered = true;
         long expected = 500;
 
         // вызываем целевой метод:
